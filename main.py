fa766df0f3392fbda9abaf91309677f8da40ba6e
Joseph Redfern <joseph.redfern@mwrinfosecurity.com>
2013-11-29 14:33:31 +0000

    workings with some wally....

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
 import cv2
 
-cv2.namedWindow("Webcam Feed", cv2.CV_WINDOW_AUTOSIZE)
-#vc = cv2.VideoCapture(0)
+cv2.namedWindow("TheresWally")
+vc = cv2.VideoCapture(0)
 
-#vc.set(3, 640)
-#vc.set(4, 480)
+rval, frame = vc.read()
 
-needle = cv2.imread("WallyFace.jpg")
-haystack = cv2.imread("WallyStack.jpg")
+me = cv2.imread("wally.png")
 
 xoff = 0
 yoff = 0
 
-scores = cv2.matchTemplate(haystack, needle, cv2.TM_SQDIFF_NORMED)
-minVal,maxVal,minLoc,maxLoc = cv2.minMaxLoc(scores)
-
-if minVal < 0.40:
-    cv2.circle(haystack, minLoc, 40, (0, 0, 255), 10)
-#    cv2.putText(haystack, "Wally?", minLoc, fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1, color=(20, 20, 155))
-else:
-    print minVal
-    haystack = scores
-
-
-newx,newy = haystack.shape[1]/3,haystack.shape[0]/3 #new size (w,h)
-newimage = cv2.resize(haystack ,(newx,newy))
-cv2.imshow("Webcam Feed", newimage)
-
 while True:
-    if cv2.waitKey(0) & 0xFF == ord('q'):
+    if rval:
+        scores = cv2.matchTemplate(frame, me, cv2.TM_SQDIFF_NORMED)
+        minVal,maxVal,minLoc,maxLoc = cv2.minMaxLoc(scores)
+
+        if minVal < 0.2:
+            cv2.rectangle(frame, minLoc, (minLoc[0]+40+xoff , minLoc[1]+40+yoff), thickness=4 ,color=(0 , 0, 255))
+            cv2.putText(frame, "Wally?", (minLoc[0]+xoff+10, minLoc[1]+yoff+60), fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1, color=(0, 0, 255))
+        else:
+            print minVal
+        cv2.imshow("TheresWally", frame)
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == 81:
+        yoff += 5
+        print "x: %s, y: %s" % (xoff, yoff)
+    elif key == 83:
+        yoff -= 5
+        print "x: %s, y: %s" % (xoff, yoff)
+    elif key == 85:
+        xoff += 5
+        print "x: %s, y: %s" % (xoff, yoff)
+    elif key == 84:
+        xoff -= 5
+        print "x: %s, y: %s" % (xoff, yoff)
+    elif key == ord('q'):
         break
+    rval, frame = vc.read()
